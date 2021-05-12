cdef5e2012092029205ad7bc71b3d42816f85ca4
mrcos-g <67390524+mrcos-g@users.noreply.github.com>
2021-05-12 09:52:34 -0400

    configure webpack typescript bundle

---
+++ webpack.common.ts
@@ -0,0 +1,29 @@
+import webpack from 'webpack';
+import { resolve } from 'path';
+import nodeExternals from 'webpack-node-externals';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+
+const commonConfig: webpack.Configuration = {
+  entry: './src/index.ts',
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.ts$/,
+        use: ['ts-loader'],
+      },
+    ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: resolve(__dirname, 'build'),
+  },
+  plugins: [new CleanWebpackPlugin()],
+  resolve: {
+    extensions: ['.mjs', '.js', '.ts'],
+  },
+  target: 'node',
+};
+
+export default commonConfig;
