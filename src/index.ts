173fdac6f13f4ef09c378288d70826b4062b7f59
mrcos-g <67390524+mrcos-g@users.noreply.github.com>
2021-05-12 21:01:37 -0400

    set up basic apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log('testing webpack bundle');
+import { ApolloServer, gql } from 'apollo-server-express';
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello() {
+      return 'world';
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.applyMiddleware({ app });
+
+app.listen({ port }, () => {
+  console.log(`Server ready at http://localhost:${port}${server.graphqlPath}`);
+});
