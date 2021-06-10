d21458550707cc7e2098e00ec79699b2d8c96fbb
mrcos-g <67390524+mrcos-g@users.noreply.github.com>
2021-06-10 09:07:20 -0400

    convert project to only work as gateway

--- src/services/accounts/schema/index.ts
+++
@@ -1,6 +0,0 @@
-import { HelloResolver } from './queries/hello';
-import { buildFederatedSchema } from '../../../utils/buildFederatedSchema';
-import { GraphQLSchema } from 'graphql';
-
-export const buildAccountsSchema = async (): Promise<GraphQLSchema> =>
-  await buildFederatedSchema({ resolvers: [HelloResolver] });
--- src/services/accounts/schema/queries/hello/hello.ts
+++
@@ -1,9 +0,0 @@
-import { Resolver, Query } from 'type-graphql';
-
-@Resolver()
-export class HelloResolver {
-  @Query(() => String)
-  public async hello(): Promise<string> {
-    return 'hello world';
-  }
-}
