1cf946726468160bcdf02eaa1ef27af79811e678
mrcos-g <67390524+mrcos-g@users.noreply.github.com>
2021-07-02 14:23:49 -0400

    Use buildService function to include our user HTTP header in every request the gateway sends to a subgraph

--- src/config/apollo.ts
+++ src/config/apollo.ts
@@ -1,11 +1,27 @@
 import { ApolloServer } from 'apollo-server-express';
-import { ApolloGateway } from '@apollo/gateway';
+import { ApolloGateway, RemoteGraphQLDataSource } from '@apollo/gateway';
 import app from './app';
+
+export interface IContext {
+  user: string;
+}
 
 const startServer = async (): Promise<void> => {
   const serviceList = [{ name: 'accounts', url: `http://localhost:4001` }];
 
-  const gateway = new ApolloGateway({ serviceList });
+  const gateway = new ApolloGateway({
+    serviceList,
+    buildService({ url }) {
+      return new RemoteGraphQLDataSource<IContext>({
+        url,
+        willSendRequest({ request, context }) {
+          if (request && request.http) {
+            request.http.headers.set('user', context.user ? (JSON.stringify(context.user) as string) : '');
+          }
+        },
+      });
+    },
+  });
 
   const server = new ApolloServer({
     gateway,
